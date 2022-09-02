398e7ff77a8e4ab55166ba3886cfc34a8f68c6ef
Denis Tarasov <taras.baldej@gmail.com>
2022-09-02 19:01:04 +0300

    header mob

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
 $(function () {
-
+  $('.header__menu-burger').click(function(event) {
+    $('.header__menu-burger,.header__nav').toggleClass('active');
+  });
 })
+
+// $('.header__box').css('height') = $('.header__image').outerHeight();
