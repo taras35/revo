ed77f39a698df1a584792bd86706479042c6bccf
Denis Tarasov <taras.baldej@gmail.com>
2022-09-02 22:28:25 +0300

    header mob

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,24 @@ $(function () {
   $('.header__menu-burger').click(function(event) {
     $('.header__menu-burger,.header__nav').toggleClass('active');
   });
+
+  if ($(window).width() < 769) {
+    $('.header__logo').appendTo($('.header__mob-container .container'))
+    $('.header__menu').appendTo($('.header__mob-container .container'))
+    // $('.header__content').appendTo($('.header__image'))
+  }
+
+
 })
 
-// $('.header__box').css('height') = $('.header__image').outerHeight();
+const headerMobile = document.querySelector('.header__mob-container');
+const callback = function(entries, observer) {
+  if (entries[0].isIntersecting) {
+    headerMobile.classList.remove('scroll')
+  } else {
+    headerMobile.classList.add('scroll')
+  }
+}
+
+const headerObserver = new IntersectionObserver(callback);
+headerObserver.observe(headerMobile);
