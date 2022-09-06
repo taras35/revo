83b84e0de4da38b4033365524d00233b24aa0b94
Denis Tarasov <taras.baldej@gmail.com>
2022-09-06 18:07:05 +0300

    choose slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,28 @@
-$(function () {
+$(document).ready(function () {
   $('.header__menu-burger').click(function(event) {
     $('.header__menu-burger,.header__nav').toggleClass('active');
   });
 
+  $('.choose__slider-wrapper').slick({
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          rows: 2,
+        }
+      },
+      {
+        breakpoint: 319,
+        settings: {
+          arrows: false,
+          dots: true,
+        }
+      },
+    ],
+  });
 })
 
 //Добавить класс хедеру при скролле
@@ -20,9 +40,9 @@ const headerObserver = new IntersectionObserver(callback);
 headerObserver.observe(headerMobile);
 
 //slider-swiper
-const swiper = new Swiper('.choose__slider', {
-  navigation: {
-    nextEl: '.choose__slider-arrow-next',
-  },
-  slidesPerView: 2,
-});
+// const swiper = new Swiper('.choose__slider', {
+//   navigation: {
+//     nextEl: '.choose__slider-arrow-next',
+//   },
+//   slidesPerView: 2,
+// });
