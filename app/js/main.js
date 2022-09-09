8ccca3d610fa860e7dc1c45fed8fdbeaea673c61
Denis Tarasov <taras.baldej@gmail.com>
2022-09-09 17:53:14 +0300

    combo mob and desk

--- app/js/main.js
+++ app/js/main.js
@@ -43,27 +43,58 @@ $(document).ready(function () {
 
   $('.giftset__trigger-item:first').click();
 
+  //combo
 
+  $('.combo__slider-wrapper').slick({
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 319,
+        settings: {
+          arrows: false,
+          dots: true,
+        }
+      },
+    ],
+  });
 });
 
 //Добавить класс хедеру при скролле
 
-const headerMobile = document.querySelector('.header__mob-container');
-const callback = function(entries, observer) {
-  if (entries[0].isIntersecting) {
-    headerMobile.classList.remove('scroll')
-  } else {
-    headerMobile.classList.add('scroll')
+window.addEventListener('DOMContentLoaded', () => {
+  //Добавить класс хедеру при скролле
+
+  const headerMobile = document.querySelector('.header__mob-container');
+  const callback = function(entries, observer) {
+    if (entries[0].isIntersecting) {
+      headerMobile.classList.remove('scroll')
+    } else {
+      headerMobile.classList.add('scroll')
+    }
+  };
+  const headerObserver = new IntersectionObserver(callback);
+
+  headerObserver.observe(headerMobile);
+
+  // Ограничеение текста в combo__text
+
+  function truncate(el, maxSize = 72) {
+    let str = el.textContent;
+    str.length > maxSize ? el.textContent = str.slice(0, maxSize) + '...' : el.textContent = str
   }
-};
-const headerObserver = new IntersectionObserver(callback);
 
-headerObserver.observe(headerMobile);
+  for (let el of document.querySelectorAll('.combo__text')) {
+    truncate(el)
+  }
 
-//slider-swiper
-// const swiper = new Swiper('.choose__slider', {
-//   navigation: {
-//     nextEl: '.choose__slider-arrow-next',
-//   },
-//   slidesPerView: 2,
-// });
+  // popup combo
+
+  
+})
